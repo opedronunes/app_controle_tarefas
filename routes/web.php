a28e0879f6c03d1ceb1252c29356a8755c419e71
Pedro Nunes <adm.pedronunes@hotmail.com>
2023-01-28 23:46:00 -0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Http\Controllers\TarefaController;
+use App\Mail\MensagemTesteMail;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('tarefa', TarefaController::class);
+
+Route::get('/mensagem-teste', function(){
+    return new MensagemTesteMail();
+    //Mail::to('adm.pedronunes@gmail.com')->send(new MensagemTesteMail());
+    //return 'Email enviado com sucesso!';
+});
