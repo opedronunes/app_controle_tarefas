f7235837170a43c936a18ab9fa34452c7f1c5be4
Pedro Nunes <adm.pedronunes@hotmail.com>
2023-01-30 23:46:14 -0300

    Criação da classe de modificação de senha enviado por email

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Notifications\RedefinirSenhaNotification;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -41,4 +43,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function sendPasswordResetNotification($token){
+        $this->notify(new RedefinirSenhaNotification($token, $this->email, $this->name));
+    }
 }
